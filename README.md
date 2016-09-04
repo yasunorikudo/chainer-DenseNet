e50066ab54435c21edfe56fba8b86319544dd86c
yasunorikudo <kchnk0@gmail.com>
2016-09-04 06:02:58 +0000

    Add hyperparam

--- README.md
+++ README.md
@@ -8,18 +8,3 @@ This repository includes network definition scripts only.
 # Requirements
 
 - [Chainer 1.5+](https://github.com/pfnet/chainer) (Neural network framework)
-
-
-# Training for cifar-10
-
-Use [mitmul's repository](https://github.com/mitmul/chainer-cifar10/tree/34aa43ff1f1bc559b02bdfc7953747aa84048612).
-
-Run:
-```
-git clone https://github.com/mitmul/chainer-cifar10.git
-cd chainer-cifar10
-git checkout 34aa43ff1f1bc559b02bdfc7953747aa84048612
-cp ../densenet.py models
-bash download.sh
-python train.py --model models/densenet.py --batchsize 64 --lr 0.1 --epoch 300 --lr_decay_freq 120
-```
--- densenet.py
+++ densenet.py
@@ -10,10 +10,10 @@ import numpy as np
 
 
 class DenseBlock(chainer.Chain):
-    def __init__(self, in_ch, growth_rate, layer):
-        self.layer = layer
+    def __init__(self, in_ch, growth_rate, n_layer):
+        self.n_layer = n_layer
         super(DenseBlock, self).__init__()
-        for i in range(self.layer):
+        for i in range(self.n_layer):
             W = np.random.randn(growth_rate, in_ch + i * growth_rate, \
                 3, 3).astype(np.float32) * math.sqrt(2. / 9 / growth_rate)
             self.add_link('bn%d' % (i + 1),
@@ -22,10 +22,10 @@ class DenseBlock(chainer.Chain):
                           L.Convolution2D(in_ch + i * growth_rate,
                                           growth_rate, 3, 1, 1, initialW=W))
 
-    def __call__(self, x, train):
-        for i in range(1, self.layer + 1):
+    def __call__(self, x, dropout_ratio, train):
+        for i in range(1, self.n_layer + 1):
             h = F.relu(self['bn%d' % i](x, test=not train))
-            h = F.dropout(self['conv%d' % i](h), 0.2, train=train)
+            h = F.dropout(self['conv%d' % i](h), dropout_ratio, train)
             x = F.concat((x, h))
         return x
 
@@ -38,59 +38,54 @@ class Transition(chainer.Chain):
             bn=L.BatchNormalization(in_ch),
             conv=L.Convolution2D(in_ch, in_ch, 1, initialW=W))
 
-    def __call__(self, x, train):
+    def __call__(self, x, dropout_ratio, train):
         h = F.relu(self.bn(x, test=not train))
-        h = F.dropout(self.conv(h), 0.2, train=train)
+        h = F.dropout(self.conv(h), dropout_ratio, train)
         h = F.average_pooling_2d(h, 2)
         return h
 
 
 class DenseNet(chainer.Chain):
-
-    insize = 32
-
-    def __init__(self, layer=12, growth_rate=12):
+    def __init__(self, n_layer=12, growth_rate=12,
+                 n_class=10, dropout_ratio=0.2, in_ch=16, block=3):
         """
 
-        layer: Number of convolution layers in one dense block.
-            If layer=12, the network is made out of 40 (12*3+4) layers.
-            If layer=32, the network is made out of 100 (32*3+4) layers.
+        n_layer: Number of convolution layers in one dense block.
+            If n_layer=12, the network is made out of 40 (12*3+4) layers.
+            If n_layer=32, the network is made out of 100 (32*3+4) layers.
         growth_rate: Number of output feature maps of each convolution layer
             in dense blocks, which is difined as k in the paper.
+        n_class: Output class.
+        dropout_ratio: Dropout ratio.
+        in_ch: Number of output feature maps of first convolution layer.
+        block: Number of dense block.
+
 
         """
-        in_chs = range(16, 16 + 4 * layer * growth_rate, layer * growth_rate)
-        W = np.random.randn(in_chs[0], 3, 3, 3).astype(np.float32) * \
-            math.sqrt(2. / 9 / in_chs[0])
-        super(DenseNet, self).__init__(
-            conv1=L.Convolution2D(3, in_chs[0], 3, 1, 1, initialW=W),
-            dense2=DenseBlock(in_chs[0], growth_rate, layer),
-            trans2=Transition(in_chs[1]),
-            dense3=DenseBlock(in_chs[1], growth_rate, layer),
-            trans3=Transition(in_chs[2]),
-            dense4=DenseBlock(in_chs[2], growth_rate, layer),
-            bn4=L.BatchNormalization(in_chs[3]),
-            fc5=L.Linear(in_chs[3], 10))
+        in_chs = range(in_ch, in_ch + (block + 1) * n_layer * growth_rate,
+                       n_layer * growth_rate)
+        W = np.random.randn(in_ch, 3, 3, 3).astype(np.float32) * \
+            math.sqrt(2. / 9 / in_ch)
+        super(DenseNet, self).__init__()
+        self.add_link('conv1', L.Convolution2D(3, in_ch, 3, 1, 1, initialW=W))
+        for i in range(block):
+            self.add_link('dense%d' % (i + 2),
+                          DenseBlock(in_chs[i], growth_rate, n_layer))
+            if not i == block - 1:
+                self.add_link('trans%d' % (i + 2), Transition(in_chs[i + 1]))
+        self.add_link('bn%d' % (block + 1), L.BatchNormalization(in_chs[block]))
+        self.add_link('fc%d' % (block + 2), L.Linear(in_chs[block], n_class))
         self.train = True
+        self.dropout_ratio = dropout_ratio
+        self.block = block
 
-    def clear(self):
-        self.loss = None
-        self.accuracy = None
-
-    def __call__(self, x, t):
-        self.clear()
-        h = F.dropout(self.conv1(x), 0.2, train=self.train)
-        h = self.trans2(self.dense2(h, self.train), self.train)
-        h = self.trans3(self.dense3(h, self.train), self.train)
-        h = F.relu(self.bn4(self.dense4(h, self.train)))
+    def __call__(self, x):
+        h = F.dropout(self.conv1(x), self.dropout_ratio, self.train)
+        for i in range(2, self.block + 2):
+            h = self['dense%d' % i](h, self.dropout_ratio, self.train)
+            if not i == self.block + 1:
+                h = self['trans%d' % i](h, self.dropout_ratio, self.train)
+        h = F.relu(self['bn%d' % (self.block + 1)](h))
         h = F.average_pooling_2d(h, h.data.shape[2])
-        h = self.fc5(h)
-
-        if self.train:
-            self.loss = F.softmax_cross_entropy(h, t)
-            self.accuracy = F.accuracy(h, t)
-            return self.loss
-        else:
-            return h
-
-model = DenseNet()
+        h = self['fc%d' % (self.block + 2)](h)
+        return h
