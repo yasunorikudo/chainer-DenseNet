Fix to set "test" parameter of batch normalization.

densenet.py
@@ -85,7 +85,7 @@ class DenseNet(chainer.Chain):
             h = self['dense%d' % i](h, self.dropout_ratio, self.train)
             if not i == self.block + 1:
                 h = self['trans%d' % i](h, self.dropout_ratio, self.train)
-        h = F.relu(self['bn%d' % (self.block + 1)](h))
+        h = F.relu(self['bn%d' % (self.block + 1)](h, test=not self.train))
         h = F.average_pooling_2d(h, h.data.shape[2])
         h = self['fc%d' % (self.block + 2)](h)
         return h