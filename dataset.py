Fix normalization.

dataset.py
@@ -8,8 +8,10 @@ from six import moves
 
 
 class PreprocessedDataset(chainer.dataset.DatasetMixin):
-    def __init__(self, pairs, random=False):
+    def __init__(self, pairs, mean, std, random=False):
         self._pairs = pairs
+        self._mean = mean
+        self._std = std
         self._random = random
 
     def __len__(self):
@@ -21,12 +23,11 @@ class PreprocessedDataset(chainer.dataset.DatasetMixin):
         # load label data
         t = np.array(label, dtype=np.int32)
 
-        # global contrast normalization
+        # normalize data
         x = np.empty_like(image)
-        mean = np.mean(image, axis=(1, 2))
         for i in moves.range(3):
-            x[i] = image[i] - mean[i]
-            x[i] /= np.std(x[i])
+            x[i] = image[i] - self._mean[i]
+            x[i] /= self._std[i]
 
         # data augmentation
         if self._random:

train.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import chainer
+from chainer.dataset import convert
 from chainer.datasets import cifar
 from chainer import serializers
 from chainer import training
@@ -29,8 +30,12 @@ def main(args):
     elif args.dataset == 'SVHN':
         raise NotImplementedError()
 
-    train = PreprocessedDataset(train, random=args.augment)
-    test = PreprocessedDataset(test)
+    images = convert.concat_examples(train)[0]
+    mean = images.mean(axis=(0, 2, 3))
+    std = images.std(axis=(0, 2, 3))
+
+    train = PreprocessedDataset(train, mean, std, random=args.augment)
+    test = PreprocessedDataset(test, mean, std)
 
     train_iter = chainer.iterators.MultiprocessIterator(train, args.batchsize)
     test_iter = chainer.iterators.MultiprocessIterator(